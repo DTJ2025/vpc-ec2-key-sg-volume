e7f489aae529914d1c93a621e88d912043a4df05
josiane <josianedeyottaiekam@gmail.com>
2025-04-04 00:07:48 +0200

    Feat(utc-app) added code for dev env build

---
+++ vpc/version.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.79.0"
+    }
+  
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "dj-w7-terraformstatebucket"
+    key    = "qa/terraform.tfstate"
+    region = "us-east-1"
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
